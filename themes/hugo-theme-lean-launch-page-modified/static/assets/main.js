273b42d6840b4f8bc307ca647da52dd90f999619
Angeloyo <33759346+Angeloyo@users.noreply.github.com>
2023-07-01 14:12:04 +0200

    added google caché

--- themes/hugo-theme-lean-launch-page-modified/static/assets/main.js
+++ themes/hugo-theme-lean-launch-page-modified/static/assets/main.js
@@ -26,14 +26,13 @@ function limpiarUrl(url) {
         let urlDestino = 'https://archive.ph/newest/' + urlLimpia;
         window.open(urlDestino, '_blank');
     });
-  
-  //   document.getElementById('boton-removepaywall').addEventListener('click', function() {
-  //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  //         let urlActual = tabs[0].url;
-  //         let urlLimpia = limpiarUrl(urlActual);
-  //         let urlDestino = 'https://removepaywall.com/' + urlLimpia;
-  //         chrome.tabs.create({ url: urlDestino });
-  //     });
-  // });
+
+    document.getElementById('boton-gcache').addEventListener('click', function() {
+        let urlActual = document.getElementById('inputtext').value;
+        let urlLimpia = limpiarUrl(urlActual);
+        let urlDestino = 'https://webcache.googleusercontent.com/search?q=cache:' + urlLimpia;
+        window.open(urlDestino, '_blank');
+    });
+
   });
   
